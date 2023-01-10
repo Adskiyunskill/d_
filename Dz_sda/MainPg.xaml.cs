c25ab51020fc11cee200749e6e58a50948fb74ba
Adskiyunskill <64008131+Adskiyunskill@users.noreply.github.com>
2023-01-10 19:51:46 +0300

    1

---
+++ Dz_sda/MainPg.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Dz_sda
+{
+    /// <summary>
+    /// Логика взаимодействия для MainPg.xaml
+    /// </summary>
+    public partial class MainPg : Page
+    {
+        public MainPg()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new NavTe()); 
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new Heroes());
+        }
+    }
+}
---
+++ Dz_sda/NavTe.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Dz_sda
+{
+    /// <summary>
+    /// Логика взаимодействия для NavTe.xaml
+    /// </summary>
+    public partial class NavTe : Page
+    {
+        public NavTe()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new Jamal());
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new Livs());
+        }
+    }
+}
